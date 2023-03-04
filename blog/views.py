5fb5996c516cd464160a7b697897ab2485763cfa
Taraansh <taraanshnandwani123@gmail.com>
2023-03-04 11:30:53 +0530

    Added templates and search view

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home")
+    return render(request, 'index.html')
 
 
 def blog(request):
-    return HttpResponse("This is blog")
-
+    return render(request, 'bloghome.html')
 
 def blogpost(request, slug):
     return HttpResponse(f"you are viewing {slug}")
 
 
+def search(request):
+    return render(request, 'search.html')
+
+
 def contact(request):
-    return HttpResponse("This is contact page")
+    return render(request, 'contact.html')
