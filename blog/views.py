a1e311d325d5ae817351b0f134ae191a4e5a9beb
Taraansh <taraanshnandwani123@gmail.com>
2023-03-06 00:35:12 +0530

    Added prev and next buttons

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Blog
+import math
 
 # Create your views here.
 def home(request):
@@ -8,8 +9,29 @@ def home(request):
 
 
 def blog(request):
+    no_of_posts = 3
+    page = request.GET.get('page')
+    if page is None:
+        page = 1
+    else:
+        page = int(page)
+
     blogs = Blog.objects.all()
-    context = {'blogs': blogs}
+    length = len(blogs)
+    blogs = Blog.objects.all()[(page - 1) * no_of_posts : page * no_of_posts]
+    
+    if page > 1:
+        prev = page - 1
+    else:
+        prev = None
+    
+    if page <= math.ceil(length/no_of_posts):
+        nxt = page + 1
+    else:
+        nxt = None
+    print(prev, nxt)
+
+    context = {'blogs': blogs, 'prev': prev, 'next': nxt}
     return render(request, 'bloghome.html', context)
 
 
