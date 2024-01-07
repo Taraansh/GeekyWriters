b3b219df4450889d6b173781b471c055c584fcbf
Taraansh <taraanshnandwani123@gmail.com>
2024-01-07 23:38:39 +0530

    Added view to add blog

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from blog.models import Blog, Contact
 import math
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'index.html')
 
@@ -18,13 +20,13 @@ def blog(request):
 
     blogs = Blog.objects.all()
     length = len(blogs)
-    blogs = Blog.objects.all()[(page - 1) * no_of_posts : page * no_of_posts]
-    
+    blogs = Blog.objects.all()[(page - 1) * no_of_posts: page * no_of_posts]
+
     if page > 1:
         prev = page - 1
     else:
         prev = None
-    
+
     if page < math.ceil(length/no_of_posts):
         nxt = page + 1
     else:
@@ -35,7 +37,7 @@ def blog(request):
 
 
 def blogpost(request, slug):
-    blog = Blog.objects.filter(slug = slug).first()
+    blog = Blog.objects.filter(slug=slug).first()
     context = {'blog': blog}
     return render(request, 'blogpost.html', context)
 
@@ -54,3 +56,18 @@ def contact(request):
         ins.save()
         print("written to database")
     return render(request, 'contact.html')
+
+
+def addblog(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        short_desc = request.POST['short_desc']
+        desc = short_desc.split()
+        slug = '-'.join(desc)
+        blog = Blog(title=title, content=content,
+                    short_desc=short_desc, slug=slug)
+        blog.save()
+        print('blog is saved')
+        return redirect('/blog/')
+    return render(request, 'addblog.html')
