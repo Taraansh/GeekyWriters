4c5d14f56ec7852235b5f7d474bd306479abdda7
Taraansh <taraanshnandwani123@gmail.com>
2024-01-08 13:25:11 +0530

    Added hosts

--- GeekyWriters/settings.py
+++ GeekyWriters/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&4t_%jl#fon&84+7)61n=23xeajabw4m-#vhh@=f^-p+9ywyfe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
